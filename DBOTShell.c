3a91fe2ded3041de2414678b42c3bdea93b50ae5
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-03-19 04:02:21 +0530

    Add files via upload

--- DBOTShell.c
+++ DBOTShell.c
@@ -1,24 +1,36 @@
-#include<stdio.h> // define the header file 
-#include<windows.h> 
+#include <stdio.h>
+#include <windows.h>
 #include <stdlib.h>
-void main()   // define the main function  
-{  
+void main()
+{
     int i;
     printf("\n\n");
-    for(i=0;i<72;i++){
+    for (i = 0; i < 72; i++)
+    {
         printf("=");
     }
-    printf("D'BOT Shell");  // print the statement.  
-    for(i=0;i<73;i++){
+    printf("D'BOT Shell"); 
+    for (i = 0; i < 73; i++)
+    {
         printf("=");
     }
+    //===============================================displaying top introduction
     printf("\n\nThe D'BOT Shell is a command-line interpreter that provides a user interface to interact with the operating system. It is a program that takes commands from the user and executes them. The shell also provides a variety of features to help users interact with the operating system, such as file and directory management, data manipulation, etc. .\n\n");
-    for(i=0;i<156;i++){
+    for (i = 0; i < 156; i++)
+    {
         printf("=");
     }
-    Sleep(4000);
-    printf("\n\n$");
-    printf("\033[0;32m");
-    printf("_ ");
-    Sleep(100000);
-}  
+    Sleep(3000);
+    char command[1000];
+    while (command != "exit")
+    {
+        printf("\n\n$");
+        printf("\033[0;32m");
+        printf("_ ");
+        fgets(command, sizeof(command), stdin);
+
+        command[strcspn(command, "\n")] = 0;
+
+        system(command);
+    }
+}
